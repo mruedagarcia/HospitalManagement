8415d1d85f1f28dcd87ee5df5fc7bdd1c387c5a7
mruedagarcia <m.rueda6@usp.ceu.es>
2022-03-11 19:44:08 +0100

    Classes and packages done

---
+++ HospitalManagement/src/hospital/pojos/Patient.java
@@ -0,0 +1,13 @@
+package hospital.pojos;
+
+import java.io.Serializable;
+
+public class Patient implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1080532239335630835L;
+	
+
+}
---
+++ HospitalManagement/src/hospital/pojos/Symptom.java
@@ -0,0 +1,5 @@
+package hospital.pojos;
+
+public class Symptom {
+
+}
