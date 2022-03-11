4f085a21018fdaa42132ef53475bb52231f6cf00
mruedagarcia <m.rueda6@usp.ceu.es>
2022-03-11 19:49:49 +0100

    Attributes of patient

--- HospitalManagement/src/hospital/pojos/Patient.java
+++ HospitalManagement/src/hospital/pojos/Patient.java
@@ -1,6 +1,7 @@
 package hospital.pojos;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class Patient implements Serializable{
 
@@ -8,6 +9,11 @@ public class Patient implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -1080532239335630835L;
-	
+	private Integer id;
+	private String email;
+	private LocalDate dob;//??
+	private Integer phone;
+	private String name;
+	private String status;
 
 }
